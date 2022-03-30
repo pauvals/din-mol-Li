9fdf691d6cc19ff362688ef15780e56062e673a1
korra <67065594+pauvals@users.noreply.github.com>
2022-03-30 10:02:24 -0300

    Pequeños cambios en test del programa.

--- examples/test.sh
+++ examples/test.sh
@@ -3,12 +3,15 @@
 # ../dana 
 # diff <(tail -n 3638 Li.xyz) Li_ref.xyz > /dev/null && echo OK || echo FAIL
 
-../dana 
-diff <(tail -n 1206 Li.xyz) ermak_piston_ref.xyz > /dev/null && echo Ok_Ermak || echo FAIL_Ermak
+echo -e "Corriendo test de Ermak con pistón de partículas. Data en \e[1mdata_ermak\e[0m =)"
+../dana > data_ermak 
+diff <(tail -n 1206 Li.xyz) ermak_piston_ref.xyz > /dev/null && echo -e "\e[1;103mOk_Ermak\e[0m" || echo -e"\e[1;101mFAIL_Ermak\e[0m"
+
+echo -e "Corriendo test de Browniana con agregado de partículas. Data en \e[1mdata_brown\e[0m =)"
 
 sed -i 's/true/false/g' movedor.ini
 
-../dana 
-diff <(tail -n 1206 Li.xyz) brownian_chunk_ref.xyz > /dev/null && echo Ok_Brown || echo FAIL_Brown
+../dana > data_brown
+diff <(tail -n 1206 Li.xyz) brownian_chunk_ref.xyz > /dev/null && echo -e "\e[1;103mOk_Brown\e[0m" || echo -e "\e[1;101mFAIL_Brown\e[0m"
 
 sed -i 's/false/true/g' movedor.ini
