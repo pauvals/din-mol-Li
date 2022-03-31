4934256e3e44348c164c707c857f1511dc0955cd
S. Alexis Paz <alexis.paz@gmail.com>
2022-03-31 16:54:02 -0300

    Update on ran(idum)
    Conversion to dp

--- dana.F90
+++ dana.F90
@@ -1110,14 +1110,14 @@ end function gasdev
  
 function ran(idum)
 implicit none
-integer, intent(inout) :: idum
 integer, parameter     :: k4b=selected_int_kind(9)
-real                   :: ran
+integer, intent(inout) :: idum
+real(dp)               :: ran
 integer, parameter     :: ia=16807,im=2147483647,iq=127773,ir=2836
-real, save             :: am
-integer(k4b), save     :: ix=-1,iy=-1,k
+real(dp), parameter    :: am=nearest(1.0,-1.0)/real(im,kind=dp)
+integer(k4b)           :: k
+integer(k4b), save    :: ix=-1,iy=-1
 if (idum <= 0 .or. iy < 0) then
-  am=nearest(1.0,-1.0)/im
   iy=ior(ieor(888889999,abs(idum)),1)
   ix=ieor(777755555,abs(idum))
   idum=abs(idum)+1
@@ -1128,7 +1128,7 @@ ix=ieor(ix,ishft(ix,5))
 k=iy/iq
 iy=ia*(iy-k*iq)-ir*k
 if (iy < 0) iy=iy+im
-ran=am*ior(iand(im,ieor(ix,iy)),1)
+ran=am*ior(iand(im,ieor(ix,iy)),1)   
 end function ran 
 
 end program
